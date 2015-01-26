4308012d8246d38abb22574190d3d040a46af4c1
Dustin Laurence <dustin@laurences.net>
2015-01-26 15:45:45 -0800

    && more

--- ecdsa.c
+++ ecdsa.c
@@ -125,10 +125,22 @@ gem_log_curve_point_more(gem_log_notify, "        ", cp);
 
 	memcpy(&inverse_y, &(cp->y), sizeof(bignum256));
 	bn_inverse(&inverse_y, &prime256k1);
+if (ctr <= ctr_max) {
+gem_log_more(gem_log_notify, "    inverse_y:\n");
+gem_log_bignum256_more(gem_log_notify, "        ", &inverse_y);
+}
 	memcpy(&lambda, &three_over_two256k1, sizeof(bignum256));
 	bn_multiply(&inverse_y, &lambda, &prime256k1);
+if (ctr <= ctr_max) {
+gem_log_more(gem_log_notify, "    lambda 1:\n");
+gem_log_bignum256_more(gem_log_notify, "        ", &lambda);
+}
 	bn_multiply(&(cp->x), &lambda, &prime256k1);
 	bn_multiply(&(cp->x), &lambda, &prime256k1);
+if (ctr <= ctr_max) {
+gem_log_more(gem_log_notify, "    lambda 2:\n");
+gem_log_bignum256_more(gem_log_notify, "        ", &lambda);
+}
 	memcpy(&xr, &lambda, sizeof(bignum256));
 	bn_multiply(&xr, &xr, &prime256k1);
 if (ctr <= ctr_max) {
